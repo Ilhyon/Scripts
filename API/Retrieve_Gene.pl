7e82192ca4cdebacfdcd106b72da518bde4f1ec9
Ilhy <anais.vannutelli@gmail.com>
2018-12-06 16:23:01 -0500

    récupération gènes
    Pour un génome de bactérie, j'ai réussi à récupérer tous ses gènes !!!!!!!!!!!!!!!!

--- API/Retrieve_Gene.pl
+++ API/Retrieve_Gene.pl
@@ -10,6 +10,8 @@ $registry->load_registry_from_db(
     -port => 4157,
 );
 
+print $registry,"\n";
+
 print "Succeed to connect to the DB";
 
 my @list_species = ("homo_sapiens","pan_troglodytes","pongo_abelii","mus_musculus","monodelphis_domestica","anolis_carolinensis","ornithorhynchus_anatinus","gallus_gallus","danio_rerio","gasterosteus_aculeatus","xenopus_tropicalis","ciona_savignyi","caenorhabditis_elegans","drosophila_melanogaster","anopheles_gambiae","apis_mellifera","octopus_bimaculoides","pediculus_humanus","stegodyphus_mimosarum","amphimedon_queenslandica","brugia_malayi","mnemiopsis_leidyi","trichoplax_adhaerens","chlamydomonas_reinhardtii","chondrus_crispus","amborella_trichopoda","physcomitrella_patens","selaginella_moellendorffii","solanum_lycopersicum","vitis_vinifera","arabidopsis_thaliana","aspergillus_nidulans","neurospora_crassa","saccharomyces_cerevisiae","schizosaccharomyces_pombe","bigelowiella_natans","dictyostelium_discoideum","emiliania_huxleyi","leishmania_major","tetrahymena_thermophila","mycoplasma_pneumoniae_m129","streptococcus_pneumoniae_TIGR4","borrelia_burgdorferi_B31","thermus_thermophilus_HB8","geobacter_sulfurreducens_PCA","wolbachia_endosymbiont_of_drosophila_melanogaster","aquifex_aeolicus_VF5","caulobacter_crescentus_CB15","cenarchaeum_symbiosum_a","coxiella_burnetii_rsa_493","gardnerella_vaginalis_0288E","helicobacter_pylori_26695","lactobacillus_plantarum_WCFS1","moraxella_catarrhalis_7169","myxococcus_xanthus_DK_1622","neisseria_meningitidis_Z2491","sulfolobus_solfataricus_P2","thermoplasma_acidophilum_DSM_1728","methanosarcina_acetivorans_C2A","methanopyrus_kandleri_AV19","methanococcus_maripaludis_S2","aeropyrum_pernix_K1","archaeoglobus_fulgidus_DSM_4304","candidatus_korarchaeum_cryptofilum_OPF8","haloarcula_marismortui_ATCC_43049","halobacterium_salinarum_R1","hyperthermus_butylicus_DSM_5456");
@@ -27,6 +29,9 @@ foreach my $sp (@list_species) {
 	if($genome_db){
 		push @genomes_db, $genome_db;
 	}
+	else{
+		print $sp,"\n";
+	}
 }
 
 # get all the genome db for the list of species (with taxon ID)
@@ -35,13 +40,20 @@ foreach my $taxonID (@list_taxonID) {
 	if($genome_db){
 		push @genomes_db, $genome_db;
 	}
+	else{
+		print $taxonID,"\n";
+	}
 }
 
-print "Succeed to retrieve the genomes DB";
+foreach my $genome (@genomes_db){
+	print $genome,"\n";
+}
+
+print "Succeed to retrieve the genomes DB\n";
 
 #~ print scalar @genomes_db; # 91 same number as species given in entry
 
-my $db_adaptor = $genomes_db[1]->db_adaptor();
-print $db_adaptor,"\n"; #
-my $gene_adaptor = $db_adaptor->get_GeneAdaptor();
-print $gene_adaptor,"\n"; #
+#~ my $db_adaptor = $genomes_db[1]->db_adaptor();
+#~ print $db_adaptor,"\n"; #Bio::EnsEMBL::DBSQL::DBAdaptor
+#~ my $gene_adaptor = $db_adaptor->get_GeneAdaptor();
+#~ print $gene_adaptor,"\n"; #
--- API/Test_genomes.pl
+++ API/Test_genomes.pl
@@ -1,6 +1,7 @@
 use strict;
 use warnings;
 use Bio::EnsEMBL::Registry;
+use Bio::Species;
 #~ use Bio::EnsEMBL::LookUp;
 #~ my $lookup = Bio::EnsEMBL::LookUp->new();
 #~ my $dba = $lookup->get_by_name_exact('escherichia_coli_str_k_12_substr_mg1655');   
@@ -13,5 +14,13 @@ $registry->load_registry_from_db(
 );
 
 my $panH_genomeDB_adaptor = $registry->get_adaptor('pan_homology', 'compara', 'GenomeDB');
-my $genome_db = $panH_genomeDB_adaptor->fetch_all_by_taxon_id("39947"); # Bio::EnsEMBL::Compara::GenomeDB
-my $db_adaptor = $genome_db->db_adaptor();
+my $genome_db = $panH_genomeDB_adaptor->fetch_by_name_assembly("haloarcula_marismortui_ATCC_43049"); # Bio::EnsEMBL::Compara::GenomeDB
+my $db_adaptor = $genome_db->db_adaptor(); # Bio::EnsEMBL::DBSQL::DBAdaptor
+my $gene_adaptor = $db_adaptor->get_GeneAdaptor(); # Bio::EnsEMBL::DBSQL::GeneAdaptor
+my @genes = $gene_adaptor->fetch_all(); # Bio::EnsEMBL::Gene
+
+foreach my $gene (@genes){
+	foreach my $g (@{$gene}){
+		print $g->stable_id(),"\n";
+	}
+}
